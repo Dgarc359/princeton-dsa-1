ca4d35a0e898f6acf3b07733c8aa8917f3162eb9
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-08-28 17:37:11 -0400

    work so far

---
+++ week-1/README.md
@@ -0,0 +1,13 @@
+# Week 1
+
+#### Spec
+https://coursera.cs.princeton.edu/algs4/assignments/hello/specification.php
+
+#### Compiling
+`javac <filename>.java`
+
+#### Running
+`java <filename>`
+
+#### Running with jar in classpath
+`java -cp "path/to/your/jar:." <filename>`
---
+++ week-1/hello/RandomWord.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion;
+        for (int i = 0; i < args.length; i++) {
+            if (i == 0) champion = args[i];
+
+
+        }
+    }
+}
